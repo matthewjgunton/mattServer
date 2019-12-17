9b3618eda84959acc82b409ef25816a6be4d6f58
Matthew J Gunton <mjg422@lehigh.edu>
2019-12-17 12:58:17 -0500

    cleaning up new integration

--- routes/rtEyeRemember.js
+++ routes/rtEyeRemember.js
@@ -28,26 +28,4 @@ sendRoutes.route("/read/alarm")
 sendRoutes.route("/read/records")
   .post(controller.readRecords);
 
-
-/////////////////////////
-// old
-// sendRoutes.route("/token/:tokenId")
-//   .get(controller.tokenReceived);
-//
-// sendRoutes.route("/records")
-//   .post(controller.reminderReceived);
-//   //make a get request here for the full records
-//
-// sendRoutes.route("/records/:tokenId")
-//   .get(controller.sendFullRecordsEJS);
-//
-// sendRoutes.route("/json/:tokenId")
-//   .get(controller.sendFullRecordsJSON);
-//
-// sendRoutes.route("/delete")
-//   .post(controller.deleteAlarm);
-//
-// sendRoutes.route("/treated")
-//   .post(controller.wasReminded);
-
 module.exports = sendRoutes;
